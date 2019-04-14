a63755f4c320673e8e06b049747bbbf879676378
felix <felixok@live.fr>
2019-04-14 20:34:20 +0200

    edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { NasaService } from './nasa.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'c';
+  public imgOfTheDay = '';
+  title = 'Nasa';
+
+  public service: NasaService;
+
+
+  constructor(param_service: NasaService) {
+    this.service = param_service;
+  }
+
+  public ngOnInit(): void {
+    this.service.getImageOfTheDay().subscribe(
+      (param_img: string) => {
+        this.imgOfTheDay = param_img;
+        // console.log(param_img.url)
+
+      }
+    );
+  }
 }
